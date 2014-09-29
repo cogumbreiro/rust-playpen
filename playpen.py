464569501a23fa68ef21fb58269f1580a41b42f6
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2014-09-29 16:41:17 +0100

    Refactored out the executor. Reverting to bash by default.

--- playpen.py
+++ playpen.py
@@ -2,20 +2,39 @@
 
 import subprocess
 
+def playpen(version, command, arguments, data=None):
+    return subprocess.Popen(("playpen",
+                            "root-" + version,
+                            "--mount-proc",
+                            "--user=rust",
+                            "--timeout=5",
+                            "--syscalls-file=whitelist",
+                            "--devices=/dev/urandom:r",
+                            "--memory-limit=128",
+                            "--",
+                            command) + arguments,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+
+def echo(version, command, arguments, data=None):
+    return subprocess.Popen(("echo", "version:", version,
+                            "command:",
+                            command) + arguments,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+
+def bash(version, command, arguments, data=None):
+    return subprocess.Popen(("bash", "-c",
+                            command) + arguments,
+                            stdin=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+                           
+
 def execute(version, command, arguments, data=None):
-    with subprocess.Popen(("playpen",
-                           "root-" + version,
-                           "--mount-proc",
-                           "--user=rust",
-                           "--timeout=5",
-                           "--syscalls-file=whitelist",
-                           "--devices=/dev/urandom:r",
-                           "--memory-limit=128",
-                           "--",
-                           command) + arguments,
-                           stdin=subprocess.PIPE,
-                           stdout=subprocess.PIPE,
-                           stderr=subprocess.STDOUT) as p:
+    with bash(version, command, arguments, data) as p:
         if data is None:
             out = p.communicate()[0]
         else:
