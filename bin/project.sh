ae9016b04d6c636ceb3db72283d394b9f908cff8
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2014-09-30 10:49:20 +0100

    Remove temporary files.

--- bin/project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/dash
-set -o errexit
-out=$(mktemp)
-trap "rm -f $out" EXIT
-java -jar "$1" - -project "$2" "$3" > $out
-printf '\377' # 255 in octal
-cat $out
-
--- bin/scribble.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/dash
-
-set -o errexit
-java -jar "$1" -
-printf '\377' # 255 in octal
-echo "Protocol is well-formed!"
-
