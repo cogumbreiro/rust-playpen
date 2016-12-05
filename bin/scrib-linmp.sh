b93eaa3fcf18b12d8fc125d4ec6b2ea34c0d3d4e
rhu1 <rhu@doc.ic.ac.uk>
2016-12-05 20:29:59 +0000

    Converting .sh scripts to use -inline.

--- bin/scrib-linmp.sh
+++ bin/scrib-linmp.sh
@@ -18,10 +18,12 @@ tmp=$(cat)
 #
 #echo 'module tmp;\n' | cat - /home/scribble/tmp/tmp2.scr > /home/scribble/tmp/tmp3.scr && mv /home/scribble/tmp/tmp3.scr /home/scribble/tmp/tmp.scr
 
-echo "$tmp" > /home/scribble/tmp/tmp.scr
+#echo "$tmp" > /home/scribble/tmp/tmp.scr
 
-java -classpath /home/scribble/lib/antlr-runtime-3.2.jar:/home/scribble/lib/antlr.jar:/home/scribble/lib/antlr-runtime.jar:/home/scribble/lib/commons-io.jar:/home/scribble/lib/scribble-cli.jar:/home/scribble/lib/scribble-core.jar:/home/scribble/lib/scribble-parser.jar:/home/scribble/lib/stringtemplate.jar:/home/scribble/lib/linmp-scala.jar main.Main /home/scribble/tmp/tmp.scr $2 > $out
-#/home/scribble/test/Game3.scr
+CLASSPATH='/home/scribble/lib/antlr-runtime-3.2.jar:/home/scribble/lib/antlr.jar:/home/scribble/lib/antlr-runtime.jar:/home/scribble/lib/commons-io.jar:/home/scribble/lib/scribble-cli.jar:/home/scribble/lib/scribble-core.jar:/home/scribble/lib/scribble-parser.jar:/home/scribble/lib/stringtemplate.jar:/home/scribble/lib/linmp-scala.jar'
+
+#java -cp "$CLASSPATH" main.Main /home/scribble/tmp/tmp.scr $2 > $out
+java -cp "$CLASSPATH" main.Main '-inline' "$tmp" "$2" > $out
 
 printf '\377' # 255 in octal
 #echo "Protocol is well-formed!"
